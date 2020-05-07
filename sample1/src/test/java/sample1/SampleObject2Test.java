1f5221835345fb96e7812779b8540d4fed2e4a80
huangpangpang <alan_x_zhang@yahoo.com>
2020-05-07 15:53:30 -0400

    add new test case

---
+++ sample1/src/test/java/sample1/SampleObject2Test.java
@@ -0,0 +1,12 @@
+package sample1;
+
+
+import org.junit.jupiter.api.Test;
+
+public class SampleObject2Test {
+	@Test
+	public void testPring(){
+		SampleObject so = new SampleObject();
+		so.print("Hello World 2!");
+	}
+}
