d8cd3ee6fb91893a9f7d2125d28414b1e45c51b7
huangpangpang <alan_x_zhang@yahoo.com>
2020-05-07 14:22:14 -0400

    first integrate eclipse project with git

---
+++ sample1/src/main/java/sample1/SampleObject.java
@@ -0,0 +1,7 @@
+package sample1;
+
+public class SampleObject {
+	public void print(String msg){
+		System.out.println(msg);
+	}
+}
---
+++ sample1/src/test/java/sample1/SampleObjectTest.java
@@ -0,0 +1,8 @@
+package sample1;
+
+public class SampleObjectTest {
+	public static void main(String [] args) throws Exception{
+		SampleObject so = new SampleObject();
+		so.print("Hello World1");
+	}
+}
